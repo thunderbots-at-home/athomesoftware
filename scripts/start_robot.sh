54a56cc212db10ac88f676cc4de6bcf233dfbccf
Devon Ash <noobaca2@gmail.com>
2013-09-21 15:23:26 -0700

    Scripts for starting the robot added

---
+++ scripts/start_robot.sh
@@ -0,0 +1,6 @@
+#Thunderbots@Home Software Team, University of British Columbia
+# Author: Devon Ash
+# Created: 21/09/13
+# Starts the robot
+
+
---
+++ scripts/start_sensors.sh
@@ -0,0 +1,5 @@
+#Thunderbots@Home Software Team, University of British Columbia
+# Author: Devon Ash
+# Created: 21/09/13
+# Initializes sensor topics for the robot
+
