d7ed45f57de8d8dce808307972a8c26736632eae
Devon Ash <noobaca2@gmail.com>
2013-09-23 15:37:39 -0700

    Added the automated sensor starting for webcam and included the bash scripts and launch files to do so

--- scripts/start_sensors.sh
+++ scripts/start_sensors.sh
@@ -1,7 +1,22 @@
+#!/bin/bash
 #Thunderbots@Home Software Team, University of British Columbia
 # Author: Devon Ash
 # Created: 21/09/13
 # Initializes sensor topics for the robot
+# Must be called from a directory that is inside the ROS_PACKAGE_PATH.
 
+## Path variables
+SENSORS_LAUNCH_PATH="../sensors/launch/start_sensors.launch"
 
-## Contact the launch file that starts the sensors
+## Configure the GSCAM variables for using integrated webcam
+export GSCAM_CONFIG="v4l2src device=/dev/video0 ! video/x-raw-rgb ! ffmpegcolorspace" $ rosrun gscam gscam
+
+## Launch integrated cam
+## Launch USB cam
+## Launch Microphone
+## Launch Speakers
+## Launch Kinect
+## Launch Lidar
+echo "Starting sensors for robot..."
+roslaunch $SENSORS_LAUNCH_PATH
+
