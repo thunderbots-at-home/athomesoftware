362eb2f0755da47eedd0e0dc336409abcd0b4b85
Michael Moritsugu <mmoritsugu@gmail.com>
2013-11-24 01:16:04 -0800

    Squashed commit of the following:
    commit 9c7f5ce07988c970fcd49592e1b1677574bfab9a
    Author: Michael Moritsugu <mmoritsugu@gmail.com>
    Date:   Sun Nov 24 01:00:40 2013 -0800
    
        launch file for base and paramterization of usb
    
        1) base.launch loads up the node
        2) device_file argument sets param server value
    
    commit aa20062095482f6824cb5fb2a9048398948a9fd1
    Author: Michael Moritsugu <mmoritsugu@gmail.com>
    Date:   Sat Nov 23 23:44:19 2013 -0800
    
        removed todos from read.me
    
    commit 242f6dd6eb6792ca9e692ae80a6ea1f66dfa17b3
    Author: Michael Moritsugu <mmoritsugu@gmail.com>
    Date:   Sat Nov 16 17:32:06 2013 -0800
    
        readme identifies potential issue with close()
    
    commit 131b79feda3757ba6af0080f783a3f9eebbc624b
    Author: Michael Moritsugu <mmoritsugu@gmail.com>
    Date:   Sat Nov 16 17:31:06 2013 -0800
    
        shutdown signal code written and untested
    
        shutdown signal code should handle SIGINT and XMLRPC shutdown
        compiles but hasn't been tested
    
    commit d4ff2c49081bf1cc5d71bd234ed9b9b7c9dcd403
    Author: Michael Moritsugu <mmoritsugu@gmail.com>
    Date:   Sat Nov 16 15:03:53 2013 -0800
    
        base controller: synchronous to asynchronous comm
    
        replaces the synchronous communication class for the base controller
        with an asynchronous communication class.
    
    commit ee1de880948fced8f17ad70a461e03c70406f2c1
    Author: Michael Moritsugu <mmoritsugu@gmail.com>
    Date:   Mon Nov 11 15:34:16 2013 -0800
    
        readme for base_controller with todo
    
    commit 8b82e16fb9f72c65a93b8b1d2cec6d1eaf4a1d08
    Author: Michael Moritsugu <mmoritsugu@gmail.com>
    Date:   Sun Nov 10 23:33:47 2013 -0800
    
        adds nav_msgs for odom
    
    commit 39a3dfa72dd6037d0ab9c5d20c45312d77943f1f
    Author: Michael Moritsugu <mmoritsugu@gmail.com>
    Date:   Sun Nov 10 23:33:03 2013 -0800
    
        Asynchronous libraries for use in base
    
    commit 9f8bc8d0b42a032e63ea704bdf6aa4cf9b73324d
    Author: Michael Moritsugu <mmoritsugu@gmail.com>
    Date:   Sun Nov 10 23:31:08 2013 -0800
    
        Replaces synchronous with asynchronous serial comm
    
        replaced TimeoutSerial with BufferedAsyncSerial
        objects. has not been tests yet. still needs to be tested
    
        Node also publishes to odom. However, it doesn't publish
        correct information. Still need to figure out how to
        process received odom info from microcontroller

--- talos_mechanism_controllers/base/src/base_controller_node.cpp
+++ talos_mechanism_controllers/base/src/base_controller_node.cpp
@@ -1,61 +1,126 @@
-#include <unistd.h>
+/* linux libraries */
+#include <signal.h>
+
+/* Boost thread library */
+#include <boost/thread.hpp>
 
 /* ROS libraries */
 #include "ros/ros.h"
+#include "ros/xmlrpc_manager.h"
 #include "ros/console.h"
 #include "geometry_msgs/Twist.h"
+#include "nav_msgs/Odometry.h"
+#include "std_msgs/String.h"
 
 /* Serial library */
-#include "TimeoutSerial.h"
+#include "BufferedAsyncSerial.h"
 
 namespace base_controller {
-	TimeoutSerial * serial_pointer;
+    BufferedAsyncSerial * async_serial;
+
+    /* signal-safe flag for whether shutdown is requested */
+    sig_atomic_t volatile g_request_shutdown = 0;
 }
 
-using boost::lexical_cast;
-using std::string;
-
-void cmdCallBack(const geometry_msgs::Twist::ConstPtr& msg)
+/* Replacement SIGINT handler */
+void mySigIntHandler(int sig)
 {
-	if ( ros::ok() ) {
-
-		string linear_x = lexical_cast<string>(msg->linear.x);
-		string linear_y = lexical_cast<string>(msg->linear.y);
-		string angular_z = lexical_cast<string>(msg->angular.z);
-
-		string usb_msg = linear_x + ", " + linear_y + ", " + angular_z + "\n";
-
-		ROS_INFO( "sending: %s", usb_msg.c_str() );
-
-		try {
-			TimeoutSerial serial( "/dev/arduino-mega256", 9600 );
-			serial.setTimeout( boost::posix_time::seconds(5) ); // TODO needs to be better way to do this
-			serial.writeString(usb_msg);
-			serial.close();
-		} catch ( boost::system::system_error& e ) {
-			ROS_ERROR( "Error: %s", e.what() );
-		}
-	}
+    base_controller::g_request_shutdown = 1;
+    ROS_WARN( "Received SIGINT, shutting down" );
 }
 
-int main( int argc, char **argv )
+/* Replacement "shutdown" XMLRPC callback */
+void shutdown_callback (XmlRpc::XmlRpcValue& params, XmlRpc::XmlRpcValue& result)
 {
-	ROS_INFO( "initializing program" );
-	ros::init( argc, argv, "base_controller" );
-	ros::NodeHandle n;
-	ros::Subscriber sub = n.subscribe("cmd_vel", 1000, cmdCallBack );
+    int num_params = 0;
+    if ( params.getType() == XmlRpc::XmlRpcValue::TypeArray )
+        num_params = params.size();
+    if ( num_params > 1 )
+    {
+        std::string reason = params[1];
+        ROS_WARN("Shutdown request received. Reason: %s]", reason.c_str());
+        base_controller::g_request_shutdown = 1; // Set flag
+    }
+    ROS_WARN("custom shutdown functionality goes here");
 
-	try {
-		ROS_INFO( "sending test signal" );
-		TimeoutSerial serial( "/dev/arduino-mega256", 9600 );
-		serial.setTimeout( boost::posix_time::seconds(5) );
-		serial.writeString("initializing\n");
-		serial.close();
-	} catch ( boost::system::system_error& e ) {
-		std::cout << "Error: " << e.what() << std::endl;
-		return 1;
-	}
+    result = ros::xmlrpc::responseInt(1, "", 0);
+}
 
-	ros::spin();
-	return 0;
+void cmdCallBack(const geometry_msgs::Twist::ConstPtr& msg) {
+    if ( ros::ok() ) {
+        std::string linear_x = boost::lexical_cast<std::string>(msg->linear.x);
+        std::string linear_y = boost::lexical_cast<std::string>(msg->linear.y);
+        std::string angular_z = boost::lexical_cast<std::string>(msg->angular.z);
+
+        std::string usb_msg = linear_x + ", " + linear_y + ", " + angular_z + "\n";
+
+        ROS_INFO( "sending: %s", usb_msg.c_str() );
+
+        try {
+            base_controller::async_serial->writeString( usb_msg );
+        } catch ( boost::system::system_error& e ) {
+            ROS_ERROR( "Error: %s", e.what() );
+        }
+    }
+}
+
+int main( int argc, char **argv ) {
+    ROS_INFO( "initializing base_controller" );
+    //ros::init( argc, argv, "talos_base_controller" );
+    ros::init( argc, argv, "base_controller", ros::init_options::NoSigintHandler );
+    ros::NodeHandle n;
+
+    /* Override SIGINT handler */
+    signal( SIGINT, mySigIntHandler );
+
+    /* Override XMLRPC shutdown */
+    ros::XMLRPCManager::instance()->unbind( "shutdown" );
+    ros::XMLRPCManager::instance()->bind( "shutdown", shutdown_callback );
+
+    /* create publisher and subscriber */
+    ROS_INFO( "subscribing to cmd_vel" );
+    ros::Subscriber sub = n.subscribe("cmd_vel", 1000, cmdCallBack );
+
+    ROS_INFO( "publishing to odom" );
+    ros::Publisher odom_pub = n.advertise<nav_msgs::Odometry>("odom", 50);
+    ros::Rate loop_rate(10);
+
+    try {
+
+        ROS_INFO( "initializing asynchronous serial communication" );
+        std::string device_file;
+        n.param<std::string>("device_file", device_file, "/dev/arduino-mega256");
+        ROS_INFO( "device_file: %s", device_file.c_str() );
+
+        base_controller::async_serial = new BufferedAsyncSerial( device_file , 9600 );
+        ROS_INFO( "reading serial comm buffer" );
+
+        //while ( ros::ok() ) {
+        while ( !(base_controller::g_request_shutdown) ) {
+            std::string readValue;
+            readValue = base_controller::async_serial->readStringUntil("\n");
+            
+            if (readValue != "") {
+                ROS_INFO( "received message: %s", readValue.c_str() );
+            //    std_msgs::String msg;        
+            //    msg.data = readValue;
+            //    odom_pub.publish(msg);
+            }
+
+            ros::spinOnce();
+            usleep( 100000 ); 
+        }
+
+    } catch ( boost::system::system_error& e ) {
+        ROS_ERROR ( "Error: %s", e.what() );
+        ROS_ERROR ( "closing serial connection" );
+        base_controller::async_serial->close();
+        delete base_controller::async_serial;
+        ros::shutdown();
+    }    
+
+    ROS_INFO ( "closing serial connection" );
+    base_controller::async_serial->close();
+    delete base_controller::async_serial;
+    ros::shutdown();
 }
