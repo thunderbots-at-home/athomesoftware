2a81e24637cacc36ec561e4de0832f649d361ed3
TheDash <noobaca2@gmail.com>
2014-01-23 12:05:54 -0800

    Now has following capability. May not work on hardware, but software is publishing messages. To get it running, have the arduino code uploaded and plug in the kinect then type: "roslaunch turtlebot_follower follower.launch". Should work.

---
+++ turtlebot_follower/CMakeFiles/turtlebot_follower_gencfg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/turtlebot_follower_gencfg"
+  "devel/include/turtlebot_follower/FollowerConfig.h"
+  "devel/share/turtlebot_follower/docs/FollowerConfig.dox"
+  "devel/share/turtlebot_follower/docs/FollowerConfig-usage.dox"
+  "devel/lib/python2.7/dist-packages/turtlebot_follower/cfg/FollowerConfig.py"
+  "devel/share/turtlebot_follower/docs/FollowerConfig.wikidoc"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang)
+  INCLUDE(CMakeFiles/turtlebot_follower_gencfg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
