1ece353e13db25b0c2b0ed8a4548bceb9514d39e
michael <michael@toxicseaweed-computer.(none)>
2014-02-03 23:35:26 -0800

    removes vision package that doesn't compile with catkin

--- vision/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/marco/catkin_ws/devel/include;/home/marco/catkin_ws/src/athomesoftware/vision/include/vision".split(';') if "/home/marco/catkin_ws/devel/include;/home/marco/catkin_ws/src/athomesoftware/vision/include/vision" != "" else []
-PROJECT_CATKIN_DEPENDS = "sensor_msgs;geometry_msgs;roscpp;nodelet;image_transport;opencv2;message_runtime".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "vision"
-PROJECT_SPACE_DIR = "/home/marco/catkin_ws/devel"
-PROJECT_VERSION = "0.0.0"
--- vision/devel/share/vision/cmake/vision-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(vision_MESSAGE_FILES "/home/marco/catkin_ws/src/vision/msg/RealObject.msg")
-set(vision_SERVICE_FILES "/home/marco/catkin_ws/src/vision/srv/Contains.srv;/home/marco/catkin_ws/src/vision/srv/FindObject.srv;/home/marco/catkin_ws/src/vision/srv/Match.srv;/home/marco/catkin_ws/src/vision/srv/GetObjectsInScene.srv")
