02a72162a262b9652b0c65b90c4a1b6eaf7d0df6
michael moritsugu <mmoritsugu@gmail.com>
2014-02-08 18:48:08 -0800

    Adds ArduinoMessage required for odom

---
+++ talos_mechanism_controllers/talos_base_controller/include/talos_base_controller/ArduinoMessage.h
@@ -0,0 +1,22 @@
+#ifndef ARDUINO_MESSAGE_H
+#define ARDUINO_MESSAGE_H
+
+struct ArduinoMessage 
+{
+
+	bool e_stop;
+
+  float time_stamp;
+
+	float l_motor_rpm;
+	float l_pid;
+	float l_feedback;
+
+	float r_motor_rpm;
+	float r_pid;
+	float r_feedback;
+
+};
+
+
+#endif
