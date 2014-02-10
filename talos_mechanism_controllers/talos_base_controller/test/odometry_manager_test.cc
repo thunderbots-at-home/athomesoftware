64979719373b3afdd939510a440816e2805a063e
michael moritsugu <mmoritsugu@gmail.com>
2014-02-10 14:31:31 -0800

    Adds tests for untested functions
    and cleans up existing tests

--- talos_mechanism_controllers/talos_base_controller/test/odometry_manager_test.cc
+++ talos_mechanism_controllers/talos_base_controller/test/odometry_manager_test.cc
@@ -1,5 +1,4 @@
 #include <gtest/gtest.h>
-#include <iostream>
 #include "odometry_manager.h"
 
 class OdometryManagerTest : public ::testing::Test 
@@ -12,15 +11,106 @@ TEST_F (OdometryManagerTest, TestDefaultConstructor) {
   EXPECT_EQ (odom_manager.x_global(), 0.0);
   EXPECT_EQ (odom_manager.y_global(), 0.0);
   EXPECT_EQ (odom_manager.th_global(), 0.0);
-  EXPECT_EQ (odom_manager.previous_timestamp(), 0.0);
+  EXPECT_EQ (odom_manager.previous_motor_ts(), 0.0);
 }
 
 TEST_F (OdometryManagerTest, TestGetCurrentTransform) {
-  EXPECT_TRUE (false);
+  ArduinoMessage message;
+  message.time_stamp = 100;
+  message.l_feedback = 300;
+  message.r_feedback = 300;
+
+  const double expected_left_delta = odom_manager.meters_per_tick() * message.l_feedback;
+  const double expected_right_delta = odom_manager.meters_per_tick() * message.r_feedback;
+  const double expected_distance = ( expected_left_delta + expected_right_delta ) / 2;
+  const double expected_theta_delta = ( expected_left_delta  - expected_right_delta ) / odom_manager.wheel_width();
+
+  const double expected_x_delta = expected_distance * cosf(expected_theta_delta + odom_manager.th_global());
+  const double expected_y_delta = expected_distance * sinf(expected_theta_delta + odom_manager.th_global());
+
+  const double expected_x = expected_x_delta + odom_manager.x_global();
+
+  const double expected_y = expected_y_delta + odom_manager.y_global(); 
+  const double expected_th = expected_theta_delta + odom_manager.th_global();
+
+  const double expected_z = 0.0;
+  const geometry_msgs::Quaternion expected_rotation = tf::createQuaternionMsgFromYaw(odom_manager.th_global());
+
+  odom_manager.UpdateOdometry(message);
+  geometry_msgs::TransformStamped actual = odom_manager.GetCurrentTransform();
+
+  /* test headers */
+  EXPECT_EQ ( actual.header.stamp, odom_manager.current_timestamp() );
+  EXPECT_EQ ( actual.header.frame_id, "odom" );
+  EXPECT_EQ ( actual.child_frame_id, "base_footprint" );
+  /* translation */
+  EXPECT_EQ ( actual.transform.translation.x , expected_x );
+  EXPECT_EQ ( actual.transform.translation.y , expected_y );
+  EXPECT_EQ ( actual.transform.translation.z , expected_z );
+  /* test quaternion */
+  EXPECT_EQ ( actual.transform.rotation.x, expected_rotation.x );
+  EXPECT_EQ ( actual.transform.rotation.y, expected_rotation.y );
+  EXPECT_EQ ( actual.transform.rotation.z, expected_rotation.z );
+  EXPECT_EQ ( actual.transform.rotation.w, expected_rotation.w );
 }
 
 TEST_F (OdometryManagerTest, TestGetCurrentOdom) {
-  EXPECT_TRUE (false);
+
+  ArduinoMessage message;
+  message.time_stamp = 100;
+  message.l_feedback = 300;
+  message.r_feedback = 300;
+
+  const double expected_left_delta = odom_manager.meters_per_tick() * message.l_feedback;
+  const double expected_right_delta = odom_manager.meters_per_tick() * message.r_feedback;
+  const double expected_distance = ( expected_left_delta + expected_right_delta ) / 2;
+  const double expected_theta_delta = ( expected_left_delta  - expected_right_delta ) / odom_manager.wheel_width();
+
+  const double expected_x_delta = expected_distance * cosf(expected_theta_delta + odom_manager.th_global());
+  const double expected_y_delta = expected_distance * sinf(expected_theta_delta + odom_manager.th_global());
+
+  const double expected_x = expected_x_delta + odom_manager.x_global();
+
+  const double expected_y = expected_y_delta + odom_manager.y_global(); 
+  const double expected_th = expected_theta_delta + odom_manager.th_global();
+
+  const double expected_z = 0.0;
+  const geometry_msgs::Quaternion expected_rotation = tf::createQuaternionMsgFromYaw(odom_manager.th_global());
+
+  // ToSeconds(delta_t)
+  const double expected_x_vel = expected_x_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts()); 
+  const double expected_y_vel = expected_y_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts());
+  const double expected_th_vel = expected_theta_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts());
+  const double expected_z_vel = 0.0;
+  const double expected_angular_x_vel = 0.0;
+  const double expected_angular_y_vel = 0.0;
+
+  /* run methods */
+  odom_manager.UpdateOdometry(message);
+  nav_msgs::Odometry actual = odom_manager.GetCurrentOdom();
+
+  /* test headers */
+  EXPECT_EQ ( actual.header.stamp, odom_manager.current_timestamp() );
+  EXPECT_EQ ( actual.header.frame_id, "odom" );
+  EXPECT_EQ ( actual.child_frame_id, "base_footprint" );
+
+  /* test positions */
+  EXPECT_EQ ( actual.pose.pose.position.x , expected_x );
+  EXPECT_EQ ( actual.pose.pose.position.y , expected_y );
+  EXPECT_EQ ( actual.pose.pose.position.z , expected_z );
+  EXPECT_EQ ( actual.pose.pose.orientation.x, expected_rotation.x );
+  EXPECT_EQ ( actual.pose.pose.orientation.y, expected_rotation.y );
+  EXPECT_EQ ( actual.pose.pose.orientation.z, expected_rotation.z );
+  EXPECT_EQ ( actual.pose.pose.orientation.w, expected_rotation.w );
+
+  /* test velocities */
+  EXPECT_EQ ( actual.twist.twist.linear.x, expected_x_vel );
+  EXPECT_EQ ( actual.twist.twist.linear.y, expected_y_vel );
+  EXPECT_EQ ( actual.twist.twist.linear.z, expected_z_vel );
+  EXPECT_EQ ( actual.twist.twist.angular.x, expected_angular_x_vel );
+  EXPECT_EQ ( actual.twist.twist.angular.y, expected_angular_y_vel );
+  EXPECT_EQ ( actual.twist.twist.angular.z, expected_th_vel );
+
 }
 
 TEST_F (OdometryManagerTest, TestToSecond){
@@ -36,13 +126,10 @@ TEST_F (OdometryManagerTest, TestForward) {
   message.l_feedback = 300;
   message.r_feedback = 300;
 
-  
-
   const double expected_left_delta = odom_manager.meters_per_tick() * message.l_feedback;
   const double expected_right_delta = odom_manager.meters_per_tick() * message.r_feedback;
   const double expected_distance = ( expected_left_delta + expected_right_delta ) / 2;
   const double expected_theta_delta = ( expected_left_delta  - expected_right_delta ) / odom_manager.wheel_width();
-
 
   const double expected_x_delta = expected_distance * cosf(expected_theta_delta + odom_manager.th_global());
   const double expected_y_delta = expected_distance * sinf(expected_theta_delta + odom_manager.th_global());
@@ -52,9 +139,9 @@ TEST_F (OdometryManagerTest, TestForward) {
   const double expected_th = expected_theta_delta + odom_manager.th_global();
 
   // ToSeconds(delta_t)
-  const double expected_x_vel = expected_x_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_timestamp()); 
-  const double expected_y_vel = expected_y_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_timestamp());
-  const double expected_th_vel = expected_theta_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_timestamp());
+  const double expected_x_vel = expected_x_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts()); 
+  const double expected_y_vel = expected_y_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts());
+  const double expected_th_vel = expected_theta_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts());
 
   odom_manager.UpdateOdometry(message);
 
@@ -86,9 +173,9 @@ TEST_F (OdometryManagerTest, TestForwardLeft) {
   const double expected_th = expected_theta_delta + odom_manager.th_global();
 
   // ToSeconds(delta_t)
-  const double expected_x_vel = expected_x_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_timestamp()); 
-  const double expected_y_vel = expected_y_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_timestamp());
-  const double expected_th_vel = expected_theta_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_timestamp());
+  const double expected_x_vel = expected_x_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts()); 
+  const double expected_y_vel = expected_y_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts());
+  const double expected_th_vel = expected_theta_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts());
 
   odom_manager.UpdateOdometry(message);
 
@@ -118,10 +205,13 @@ TEST_F (OdometryManagerTest, TestRotateRightLeft) {
   const double expected_y = expected_y_delta + odom_manager.y_global(); 
   const double expected_th = expected_theta_delta + odom_manager.th_global();
 
+  const unsigned long expected_previous_motor_ts = 180;
+  const ros::Time expected_current_ts(0, 180 * 1000000);
+
   // ToSeconds(delta_t)
-  const double expected_x_vel = expected_x_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_timestamp()); 
-  const double expected_y_vel = expected_y_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_timestamp());
-  const double expected_th_vel = expected_theta_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_timestamp());
+  const double expected_x_vel = expected_x_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts()); 
+  const double expected_y_vel = expected_y_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts());
+  const double expected_th_vel = expected_theta_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts());
 
   odom_manager.UpdateOdometry(message);
   EXPECT_EQ ( odom_manager.x_global(), expected_x );
@@ -130,15 +220,13 @@ TEST_F (OdometryManagerTest, TestRotateRightLeft) {
   EXPECT_EQ ( odom_manager.x_vel(), expected_x_vel );
   EXPECT_EQ ( odom_manager.y_vel(), expected_y_vel );
   EXPECT_EQ ( odom_manager.th_vel(), expected_th_vel );
+  EXPECT_EQ ( odom_manager.previous_motor_ts(), expected_previous_motor_ts );
+  EXPECT_EQ ( odom_manager.current_timestamp(), expected_current_ts );
 
   ArduinoMessage message2;
   message2.time_stamp = 344;
   message2.l_feedback = -120;
   message2.r_feedback = 120;
-
-
-
-
 
   const double expected_left_delta2 = odom_manager.meters_per_tick() * message2.l_feedback;
   const double expected_right_delta2 = odom_manager.meters_per_tick() * message2.r_feedback;
@@ -152,10 +240,13 @@ TEST_F (OdometryManagerTest, TestRotateRightLeft) {
   const double expected_y2 = expected_y2_delta + odom_manager.y_global();
   const double expected_th2 = expected_theta_delta2 + odom_manager.th_global();
 
+  const unsigned long expected_previous_motor_ts2 = 344;
+  const ros::Time expected_current_ts2(0, 344 * 1000000);
+
   // ToSeconds(delta_t)
-  const double expected_x_vel2 = expected_x2_delta / odom_manager.ToSeconds(message2.time_stamp - odom_manager.previous_timestamp()); 
-  const double expected_y_vel2 = expected_y2_delta / odom_manager.ToSeconds(message2.time_stamp - odom_manager.previous_timestamp());
-  const double expected_th_vel2 = expected_theta_delta2 / odom_manager.ToSeconds(message2.time_stamp - odom_manager.previous_timestamp());
+  const double expected_x_vel2 = expected_x2_delta / odom_manager.ToSeconds(message2.time_stamp - odom_manager.previous_motor_ts()); 
+  const double expected_y_vel2 = expected_y2_delta / odom_manager.ToSeconds(message2.time_stamp - odom_manager.previous_motor_ts());
+  const double expected_th_vel2 = expected_theta_delta2 / odom_manager.ToSeconds(message2.time_stamp - odom_manager.previous_motor_ts());
 
   odom_manager.UpdateOdometry(message2);
 
@@ -165,6 +256,8 @@ TEST_F (OdometryManagerTest, TestRotateRightLeft) {
   EXPECT_EQ ( odom_manager.x_vel(), expected_x_vel2 );
   EXPECT_EQ ( odom_manager.y_vel(), expected_y_vel2 );
   EXPECT_EQ ( odom_manager.th_vel(), expected_th_vel2 );
+  EXPECT_EQ ( odom_manager.previous_motor_ts(), expected_previous_motor_ts2 );
+  EXPECT_EQ ( odom_manager.current_timestamp(), expected_current_ts2 );
 }
 
 
@@ -193,9 +286,9 @@ TEST_F (OdometryManagerTest, TestFromNonZeroStart) {
   const double expected_th = fmod((expected_theta_delta + odom_manager.th_global()),(2*M_PI));
 
   // ToSeconds(delta_t)
-  const double expected_x_vel = expected_x_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_timestamp()); 
-  const double expected_y_vel = expected_y_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_timestamp());
-  const double expected_th_vel = expected_theta_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_timestamp());
+  const double expected_x_vel = expected_x_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts()); 
+  const double expected_y_vel = expected_y_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts());
+  const double expected_th_vel = expected_theta_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts());
 
   odom_manager.UpdateOdometry(message);
 
@@ -226,9 +319,9 @@ TEST_F (OdometryManagerTest, TestBackRight) {
   const double expected_th = expected_theta_delta + odom_manager.th_global();
 
   // ToSeconds(delta_t)
-  const double expected_x_vel = expected_x_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_timestamp()); 
-  const double expected_y_vel = expected_y_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_timestamp());
-  const double expected_th_vel = expected_theta_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_timestamp());
+  const double expected_x_vel = expected_x_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts()); 
+  const double expected_y_vel = expected_y_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts());
+  const double expected_th_vel = expected_theta_delta / odom_manager.ToSeconds(message.time_stamp - odom_manager.previous_motor_ts());
 
   odom_manager.UpdateOdometry(message);
 
@@ -238,6 +331,34 @@ TEST_F (OdometryManagerTest, TestBackRight) {
   EXPECT_EQ ( odom_manager.x_vel(), expected_x_vel );
   EXPECT_EQ ( odom_manager.y_vel(), expected_y_vel );
   EXPECT_EQ ( odom_manager.th_vel(), expected_th_vel ); 
+}
+
+TEST_F (OdometryManagerTest, TestUpdateTime) {
+  ArduinoMessage message;
+  message.time_stamp = 1000;
+  message.l_feedback = 0;
+  message.r_feedback = 0;
+
+  const double expected_x = 0.0;
+  const double expected_y = 0.0;
+  const double expected_th = 0.0;
+  const double expected_x_vel = 0.0;
+  const double expected_y_vel = 0.0;
+  const double expected_th_vel = 0.0;
+  const double expected_previous_motor_ts = 1000;
+  const ros::Time expected_current_ts(1);
+
+  odom_manager.UpdateOdometry(message);
+
+  EXPECT_EQ ( odom_manager.x_global(), expected_x );
+  EXPECT_EQ ( odom_manager.y_global(), expected_y );
+  EXPECT_EQ ( odom_manager.th_global(), expected_th );
+  EXPECT_EQ ( odom_manager.x_vel(), expected_x_vel );
+  EXPECT_EQ ( odom_manager.y_vel(), expected_y_vel );
+  EXPECT_EQ ( odom_manager.th_vel(), expected_th_vel ); 
+
+  EXPECT_EQ ( odom_manager.previous_motor_ts(), expected_previous_motor_ts );
+  EXPECT_EQ ( odom_manager.current_timestamp(), expected_current_ts );
 }
 
 int main (int argc, char **argv) {
