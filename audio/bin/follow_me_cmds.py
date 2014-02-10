d19b90e9c297c125e863dc9c57210887e6ed4152
michael moritsugu <mmoritsugu@gmail.com>
2014-02-09 16:10:37 -0800

    Removes say() methods from main loop

--- audio/bin/follow_me_cmds.py
+++ audio/bin/follow_me_cmds.py
@@ -6,7 +6,6 @@ voice_cmd_vel.py is a simple demo of speech recognition.
   in the corpus file.
 """
 
-from sound_play.libsoundplay import SoundClient
 import rospy
 import math
 
@@ -24,16 +23,13 @@ class follow_me_cmds:
 
         #Subscribe to the microphone input. 
         rospy.Subscriber('recognizer/output', String, self.speechCb)
-        self.soundhandle = SoundClient()
         rospy.sleep(1)
-        self.soundhandle.stopAll()
         rospy.sleep(2)
-        self.soundhandle.say("I'm operational.")
         rospy.sleep(2)
 
         r = rospy.Rate(10.0)
         while not rospy.is_shutdown():
-            #self.pub_.publish(self.msg)
+            #pub_.publish(self.msg)
             r.sleep()
         
     def speechCb(self, msg):
@@ -45,14 +41,12 @@ class follow_me_cmds:
 	## Issue following command
         rospy.sleep(1)
         if msg.data.find("are you ready") > -1:
-            self.soundhandle.say("I am ready commander")
-	     #self.msg.data = "REDY BRO" ##TODO Add code that confirms readiness here
+            pass
+            #self.msg.data = "REDY BRO" ##TODO Add code that confirms readiness here
         if msg.data.find("remember me") > -1:
             follow_me_cmds.ask_for_name = True
-            self.soundhandle.say("Tell me your name")
         elif follow_me_cmds.ask_for_name == True and msg.data in namelist:
             rospy.sleep(1)
-            self.soundhandle.say("Ok. I believe your name is" + msg.data)
 
             pub = rospy.Publisher('fr_order', FRClientGoal)
             # Here we would call on the face recognition node to start learning.
@@ -65,16 +59,14 @@ class follow_me_cmds:
             follow_me_cmds.ask_for_name = False
         elif self.attempts < 3 and follow_me_cmds.ask_for_name == True:
             rospy.sleep(1)
-            self.soundhandle.say("Sorry, what is your name?")
             follow_me_cmds.attempts += 1
-                #Take the next speech input and compare it against a list of possible names.
+            #Take the next speech input and compare it against a list of possible names.
             #self.msg.data = "REMEMBER ME BRU" ##TODO Add code that remembers person here
         if msg.data.find("follow me") > -1:
-            self.soundhandle.say("I will follow you")
+           pass
             #self.msg.data = "Follow YOU? NAH!" ##TODO Add following node/execution here
 
         if msg.data.find("confirm my identity") > -1:
-            self.soundhandle.say("Identity confirmed")
             #self.msg.data = "CONFIRMD" ##TODO check identity of person here
             pub = rospy.Publisher('fr_order', FRClientGoal)
             fr_cmd = FRClientGoal()
@@ -90,7 +82,7 @@ class follow_me_cmds:
 if __name__=="__main__":
     rospy.init_node('follow_me_cmds')
     try:
-         follow_me_cmds()
+        follow_me_cmds()
     except:
         pass
 
