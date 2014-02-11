2626f123c43a9c1e55c40cd0295fa5aa737d5384
michael moritsugu <mmoritsugu@gmail.com>
2014-02-11 01:04:18 -0800

    Implemented Parser class
    1) Makes it so that parser tests build
    2) Adds missing timestamp to ArduinoMessage
    3) makes parser actually work
    4) updates tests to reflect whether parser is
    broken

--- talos_mechanism_controllers/talos_base_controller/test/parser_test.cpp
+++ talos_mechanism_controllers/talos_base_controller/test/parser_test.cpp
@@ -1,21 +1,25 @@
-#include "talos_base_controller/parser.cpp"
-#include <gtest/gtest.h>
 #include <talos_base_controller/ArduinoMessage.h>
+#include "parser.cpp"
+#include <gtest/gtest.h>
+#include <string>
 
 TEST(TestSuite, testExpectedParse)
 {
-
-
 	Parser p;
-	p.parse("1, 0.34343, 0.13413919, 0.95381513, 4.13431, 8.343, 9.0");
-
-	
+  std::string input( "100, 1, 0.34343, 0.134139, 0.95381, 4.13431, 8.343, 9.0," );
+	ArduinoMessage msg = p.parse(input);
+  EXPECT_EQ ( msg.time_stamp , 100 );
+  EXPECT_EQ ( msg.e_stop , 1 );
+  EXPECT_EQ ( msg.l_motor_rpm , 0.34343f );
+  EXPECT_EQ ( msg.r_motor_rpm , 0.134139f );
+  EXPECT_EQ ( msg.l_pid , 0.95381f );
+  EXPECT_EQ ( msg.r_pid , 4.13431f );
+  EXPECT_EQ ( msg.l_feedback , 8.343f );
+  EXPECT_EQ ( msg.r_feedback , 9.0f );
 }
-
 
 int main(int argc, char** argv)
 {
-
 	testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
 }	
