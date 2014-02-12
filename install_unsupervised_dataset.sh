effd7ca408ecb087cd6bcb04207abde8c72e4d59
TheDash <noobaca2@gmail.com>
2014-02-12 11:48:39 -0800

    Added: File todo for unsupervised dataset, modified installation deps

---
+++ install_unsupervised_dataset.sh
@@ -0,0 +1,5 @@
+#TODO 
+#Load the 1 million images from the Tiny Image Dataset (Regularly 80MImages)
+#These 1M images will be hosted on the thunderbots@Home svn in a .tar.gz
+#TODO
+#Write a script that extracts this tar and puts the 1M images in the /datasets/tinyimages/ folder.
