a876d45ba871ae7c8219baaf18e4c71576a46a20
TheDash <noobaca2@gmail.com>
2014-02-12 14:15:05 -0800

    Added: tinyimages1M.py loads the images from datasets/tinyimages/ into a DenseDesignMatrix for pylearn2 use

---
+++ talos_follower/src/tinyimages1M.py
@@ -0,0 +1,41 @@
+"""
+.. todo::
+
+    WRITEME
+"""
+import os, cPickle, logging
+_logger = logging.getLogger(__name__)
+
+
+import numpy as np
+N = np
+from pylearn2.datasets.dense_design_matrix import DenseDesignMatrix
+from pylearn2.expr.preprocessing import global_contrast_normalize
+from PIL import Image
+
+def load_data(start, stop):
+    # Loads the 1 million images into X and creates a DenseDesignMatrix
+    # for use in a Denoising Autoencoder which is later used in a sDAE. 
+    # Returns: dataset: DenseDesignMatrix(start, stop)
+    dataset_location = "~/catkin/src/athomesoftware/datasets/tinyimages/"
+    X = []
+    y = []
+
+    print("Loading images from " + dataset_location)
+    for images in os.walk(dataset_location):
+        if (images.endswith('.png')):
+            im = Image.open(images)
+            im.reshape(3, 32, 32)
+            row = list(im.getData())
+            X.append(row)
+    
+    print("Images loaded from " + dataset_location)
+    X = np.asarray(X)
+    y = np.asarray(y)
+    y = y.reshape(y.shape[0], 1)
+    X = X[start:stop, :]
+    y = y[start:stop, :]
+
+    print("Creating design matrix " + dataset_location)
+    return DenseDesignMatrix(X=X, y=y)
+        
