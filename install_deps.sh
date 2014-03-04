fb3f60c372015a6312031f391b5b99674e9d72d0
TheDash <noobaca2@gmail.com>
2014-03-04 08:13:28 -0800

    Added: moveit deps

--- install_deps.sh
+++ install_deps.sh
@@ -40,6 +40,7 @@ sudo apt-get install ros-${ROS_DISTRO}-gscam
 sudo apt-get install ros-${ROS_DISTRO}-mjpeg-server
 sudo apt-get install ros-${ROS_DISTRO}-ros-control
 sudo apt-get install ros-${ROS_DISTRO}-ros-controllers
+sudo apt-get install ros-${ROS_DISTRO}-moveit-ros
 
 #Installing Gazebo Simulator (This installs the most recent version of gazebo)
 sudo sh -c 'echo "deb http://packages.osrfoundation.org/gazebo/ubuntu precise main" > /etc/apt/sources.list.d/gazebo-latest.list'
