66efde09e2c6a53e0f1393575753392debca028f
TheDash <noobaca2@gmail.com>
2014-03-06 13:17:38 -0800

    Added: __all__ files

---
+++ talos_os/states/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['command_standby_state', 'failed_state_prompt', 'follower_command_standby_state', 'remembering_user_state', 'startup_state', 'tracking_unidentified_state']
---
+++ talos_os/states/follow_states/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ['failed_tracking_state', 'occluded_state', 'positioning_for_user_state', 'tracking_wrong_user_state', 'user_off_screen_state', 'no_user_detected_state', 'following_state']
