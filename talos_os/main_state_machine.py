15785b918d8b9f8afca6ea30cec8333fd8ab0e8f
TheDash <noobaca2@gmail.com>
2014-03-06 13:26:05 -0800

    Now intreprets

--- talos_os/main_state_machine.py
+++ talos_os/main_state_machine.py
@@ -44,10 +44,10 @@ def main():
                                 transitions={"FailedStatePrompt":'FailedStatePrompt'})
 
         # TRACKING UNIDENTIFIED USER STATE
-        smach.StateMachine.add('TrackingUnidentifiedUserState', TrackingUnidentifiedUserState(), transitions={'TrackingFailed':'FailedStatePrompt', 'IdentifiedUser':'FollowerCommandStandbyState', 'ContinueTracking':'TrackingUnidentifiedUserState'}
+        smach.StateMachine.add('TrackingUnidentifiedUserState', TrackingUnidentifiedUserState(), transitions={'TrackingFailed':'FailedStatePrompt', 'IdentifiedUser':'FollowerCommandStandbyState', 'ContinueTracking':'TrackingUnidentifiedUserState'})
         
         # FOLLOWER COMMAND STANDBY STATE
-        smach.Statemachine.add('FollowerCommandStandbyState', FollowerCommandStandbyState(), transitions={"FollowMeCommandDetected":"FollowingState", "RestartCommandDetected":"FailedStatePrompt", "ContinueStandby":"FollowerCommandStandbyState"})
+        smach.Statemachine.add('FollowerCommandStandbyState', FollowerCommandStandbyState(), transitions={'FollowMeCommandDetected':'FollowingState','RestartCommandDetected':'FailedStatePrompt', 'ContinueStandby':'FollowerCommandStandbyState'})
 
         # FOLLOWING STATE
         smach.StateMachine.add("FollowingState", FollowingState(), transitions={"ContinueFollowing":"FollowingState", "FollowingFailed":"FailedStatePrompt", "NoUserDetected":"NoUserDetectedState"})
