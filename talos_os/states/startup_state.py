785d387659d9b863d61226da289ccabcf962c5d0
TheDash <noobaca2@gmail.com>
2014-03-06 13:49:38 -0800

    Documentation

--- talos_os/states/startup_state.py
+++ talos_os/states/startup_state.py
@@ -6,16 +6,46 @@ import rospy
 import smach
 import smach_ros
 
+
+################################ DEVELOPER README #########################
+# On system startup, what should the robot be doing?
+# The robot should bring up all of the necessary components (Eg. its interfaces [speech, visual, etc], and its funcionality, and its sensors.)
+# This can be achieved by roslaunching the thunderbot_bringup.
+###########################################################################
 class StartupState(smach.State):
+
+############################### ON STATE INIT #############################
+# Initializes the robot system via talos_bringup.launch
+
+# Nodes:
+#    JointStatePublisher
+#    RobotStatePublisher
+#    Rviz
+#    hector_mapping
+#    StaticTransformPublisher
+#    move_base (from nav_stack)
+
+# Sensors:
+#    Kinect
+#    Hokuyo Laser
+#    TODO: Add microphone/speakers launch
+
+# Controllers:
+#    PS3 controller
+
 
     def __init__(self):
         smach.State.__init__(self, outcomes=["NoCommandDetected", "CommandDetected"])
         self.counter = 0
 
+################################ ON STATE EXECUTION #######################
+# On state execution, the robot should listen for the "Ok, Talos" command
+# given by the speech to text node. If this has occured, then the next state
+# will happen.
+
     def execute(self, userdata):
         rospy.loginfo("Standing by for Ok, Talos")
 
-        # TODO listen for speech to text commands that tell Talos "Ok, Talos" has been detected and set this variable true.        
         ok_talos_detected = 0
 
         if (ok_talos_detected):
