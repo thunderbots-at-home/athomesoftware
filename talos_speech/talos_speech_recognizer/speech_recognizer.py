4046fb3482ccbfba1fcd7cf3dc4dccfd1dbe2741
Devon Ash <noobaca2@gmail.com>
2014-03-06 21:14:57 -0800

    Added thangs

---
+++ talos_speech/talos_speech_recognizer/speech_recognizer.py
@@ -0,0 +1,21 @@
+## Author: Devon Ash
+## Maintainer: noobaca2@gmail.com
+
+
+
+
+def main():
+
+    rospy.init_node("speech_recognizer")
+    rospy.loginfo(rospy.get_name() + ": Started speech recognizer")
+
+    #Set up publisher that published the text it hears
+    #TODO
+
+ 
+
+
+
+
+if __name__ == "__main__":
+    
