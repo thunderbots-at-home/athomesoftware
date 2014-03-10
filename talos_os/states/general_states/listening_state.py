fd0fb52480bda50e6a22afb95078b5cb18416329
TheDash <noobaca2@gmail.com>
2014-03-09 21:25:22 -0700

    Added general listening state

--- talos_os/states/general_states/listening_state.py
+++ talos_os/states/general_states/listening_state.py
@@ -7,14 +7,39 @@
 #    The listening state is an abstraction that puts the robot into a #    pause until it has heard the phrase or utterance in the parameter #    to the class. Once heard, it will move forward onto the next state #    and before that, execute some given function. 
 #
 ########################################################################
-class ListeningState:
+# Imports
 
+import roslib
+import rospy
+import smach
+import smach_ros
+from std_msgs.msg import String
+from talos_speech.srv import ListenFor
+
+############################# CLASS DEF ################################
+class ListeningState(smach.State):
 
     def __init__(self, utterance):
         self.utterance = utterance
+        self.State.__init__(self, outcomes=["CommandDetected", "NoCommandDetected"])
+        self.counter = 0
+        rospy.loginfo("ListeningState listening for: %s", self.utterance)
 
+    def execute(self, userdata):
+        rospy.wait_for_service('listen_for')
+        response = ListenFor()
+        
+        try:
+            rospy.loginfo("Waiting for utterance: %s", self.utterance)
+            listen_for = rospy.ServiceProxy('listen_for', ListenFor)
+            response = listen_for(self.utterance)
+            if (response.result == 1):
+                return "CommandDetected"
 
+        except rospy.ServiceException, e:
+            print "Service call failed: %s" %e
 
+        return "NoCommandDetected"
 
 
 
