d2850f5fc0bc647cbe44d73fef322425fbbe8e52
TheDash <noobaca2@gmail.com>
2014-03-09 23:04:02 -0700

    Added library of voice commands

---
+++ talos_os/states/general_states/voice_command_library_state.py
@@ -0,0 +1,32 @@
+########################################################################
+## Author: Devon Ash
+## Maintainer: noobaca2@gmail.com
+######################## DEVELOPER README ##############################
+# Info
+#
+#    The voice command library takes a dictionary of
+#    utterance:state_machine pairs. On hearing an utterance, 
+#    the state machine is executed. When the state machine finishes
+#    control returns to the initial standby state where the user must
+#    access the voice command libray by first saying "OK talos" or
+#    something similar.
+#
+########################################################################
+# Imports
+
+import roslib
+import rospy
+import smach
+import smach_ros
+from std_msgs.msg import String
+from talos_speech.srv import ListenFor
+
+############################### CLASS DEF ##############################
+class VoiceCommandLibraryState(smach.State):
+
+    def __init__(self, utterances):
+        self.utterances = utterances
+        smach.State.__init__(self, outcomes=["WaitingForCommand", "CommandComplete"])
+        self.counter = 0
+        rospy.loginfo("VoiceCommandLibraryState: Commands available")
+        for 
