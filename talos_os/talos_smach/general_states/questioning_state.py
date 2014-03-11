ca2ee48ac2eb5ec1d6af537460e5ad8e43fcdd70
Devon Ash <noobaca2@gmail.com>
2014-03-11 00:48:56 -0700

    more q

--- talos_os/talos_smach/general_states/questioning_state.py
+++ talos_os/talos_smach/general_states/questioning_state.py
@@ -20,4 +20,15 @@ import smach_ros
 class QuestioningState(smach.State):
 
     def __init__(self, question):
-        
+        self.question = question
+        self.response = "NoResponseGiven"
+        smach.State.__init__(self, outcomes=["ResponseReceived", "NoResponseGiven"])
+        self.counter = 0
+        self.attempts = 3
+
+
+    # Asks the user a question via the sound-play microphone node
+    def execute(self, userdata):
+        # 3 attempts for asking a question
+        for attempt in range(self.attempts):
+            
