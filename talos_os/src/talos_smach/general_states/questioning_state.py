54084591d76f297ff9e007204ca78b367109699b
Devon Ash <noobaca2@gmail.com>
2014-03-11 23:00:36 -0700

    added functionality

--- talos_os/src/talos_smach/general_states/questioning_state.py
+++ talos_os/src/talos_smach/general_states/questioning_state.py
@@ -33,43 +33,49 @@ class QuestioningState(smach.State):
         smach.State.__init__(self, outcomes=["ResponseReceived", "NoResponseGiven"])
         self.counter = 0
         self.attempts = 3
+        self.response = "None"
+        self.waiting_for_response = False  
+
+        # Subscribe to recognizer/output
+        self.subscriber = rospy.Subscribe('recognizer/output', String, self.response_callback)
+
+    def response_callback(self, data):
+        if len(data.data) > 0 and self.waiting_for_response:
+            # Call the stop listening service
+            self.response = data.data
+            self.waiting_for_response = False
+            SpeechListener.stop_recognizer()
+        else:
+            rospy.loginfo("Sorry, I didn't hear that")
+
+    def get_response(self):
+        self.waiting_for_response = True
+        # Start
+        #Code to do response here, subscribes to /output topic. 
+        SpeechListener.start_recognizer()
+
+        return self.waiting_for_response
 
     # Asks the user a question via the sound-play microphone node
     def execute(self, userdata):
 
-        # Publish a sound_play/SoundRequest
-        # 3 attempts for asking a question
-        for attempt in range(self.attempts):
-            #
- 
-            rospy.loginfo("Asking question: %s", self.question)
-            SpeechListener.say("What is your name?")
-            rospy.loginfo("Sending sound request...")
-            # Listen for the next thing said by calling the listen_for_any service
-            rospy.sleep(4)   
-            try:
+        rospy.loginfo("Asking question: %s", self.question)
+        SpeechListener.say("What is your name?")
+        rospy.loginfo("Sending sound request...")
+        rospy.sleep(4)   
 
-                # Should pause and wait until this returns 
-                listen_for_any = rospy.ServiceProxy('listen_for_any', Empty)
-                response = listen_for_any()
+        SpeechListener.say("Is " + response[0] + " what you mean? Yes or No")
+        rospy.sleep(4)       
 
-                print "Is this thing threded"
-                # Do a confirmation check
+        while (self.get_response()):
+            rospy.sleep(2)
+            rospy.loginfo("Waiting for response...")
 
-                sound_req.arg = "Is " + response.result + " what you mean? Yes or No"
-                sound_req.command = 1
-                publisher.publish(sound_req)
-                
-                listen_for_any_two = rospy.ServiceProxy('listen_for_any', Empty)
-                response_two = listen_for_any()
+           
 
-                if (response_two.result == "yes"):
-                    ## Return that result
-                    self.response.append(response_two.result)
-                    return "ResponseReceived"
-
-            except rospy.ServiceException, e:
-                print "Service call failed %s" %e
+        if (response_two.result == "yes"):
+            self.response.append(response_two.result)
+            return "ResponseReceived"
 
         return "NoResponseGiven"
 
